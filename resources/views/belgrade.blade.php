8e80970614637462a0ce9304b4c92a2c769f6855
IgorStevkovski <IgorStevkovski@users.noreply.github.com>
2016-09-26 19:58:09 +0200

    Update belgrade.blade.php

--- resources/views/belgrade.blade.php
+++ resources/views/belgrade.blade.php
@@ -172,7 +172,7 @@
 
                     $.ajax({
                         type: "POST",
-                        url:"city/belgrade/createComment",
+                        url:"laravelProektSOA/public/city/belgrade/createComment",
                         //url:"CityController@belgradeCreateComment",
                         data: $(id).serialize(),
 
